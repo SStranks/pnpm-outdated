3a0ea2bd9eecdab97c25627a54134293552dd5cb
Simon Stranks <sstranks87@gmail.com>
2025-07-15 14:56:06 +0100

    chore(configs): amended various configs

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import { EslintConfig, ConfigPrettier } from '@packages/eslint-config';
 
 export default [
   {
-    ignores: ['**/node_modules', '**/dist', '**/__snapshots__', '**/mocks', '**/coverage'],
+    ignores: ['**/node_modules', '**/dist', '**/__snapshots__', '**/mocks', '**/coverage', 'pnpm-lock.yaml'],
   },
   {
     files: ['src/**.[jt]s'],
